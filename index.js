341f2e112e613d75755e0929b47cdaa6e2098ac4
shanette2010 <shanettewhitehead@yahoo.com>
2024-03-28 16:40:56 -0500

    updating message and correcting issues

--- index.js
+++ index.js
@@ -8,4 +8,8 @@ if (age >= 18) {
         return `Welcome, {name}!`;
 
         return `You are not old enough to enter, {name}.`;
+
+        const adultMessage = verifyAdulthood(shanette, 31);
+
+console.log(adultMessage);
         }
