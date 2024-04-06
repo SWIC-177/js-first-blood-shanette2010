486bac453018ab03ee0cfc8a7e788676d72181f4
shanette2010 <shanettewhitehead@yahoo.com>
2024-04-06 15:25:10 -0500

    taking out functions

---
+++ Bball.js
@@ -0,0 +1,17 @@
+function generate_increment_function(score_increment){
+        
+    } increment_score(score);
+        return score + score_increment
+    return increment_score
+
+
+increment_by_3 = generate_increment_function(3)
+increment_by_5 = generate_increment_function(5)
+
+
+current_score = 0
+current_score = increment_by_3(current_score)
+print("Current score after incrementing by 3:", current_score)
+
+current_score = increment_by_5(current_score)
+print("Current score after incrementing by 5:", current_score)
